6e8ea6d01595bd7c233a3d384fd201846edf3ba7
mdxacura2011 <mdx.acura2011@yandex.ua>
2016-03-20 20:32:04 +0200

    widget ItFan

---
+++ public_html/app/code/local/Itfan/Widget/Block/Widget.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sergey
+ * Date: 12.01.16
+ * Time: 9:47
+ */
+class Itfan_Widget_Block_Widget extends Mage_Core_Block_Template implements Mage_Widget_Block_Interface
+{
+    protected $_productImageUrl = array();
+
+    protected function _toHtml()
+    {
+        $category_id = Mage::registry('current_category')->getId();
+
+        $products = Mage::getModel('catalog/product')->getCollection()
+        ->addAttributeToSelect('image')
+        ->addCategoryFilter(Mage::getModel('catalog/category')->load($category_id));
+        $products->getSelect()->limit(3);
+         $products->load();
+        foreach($products as $product) {
+           $this->_productImageUrl[] = $product;
+        }
+
+        return parent::_toHtml();
+    }
+}
